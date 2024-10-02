1e6f520ed92a1e77d86319e8e5db098ddbe1629f
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-10-03 00:28:59 +0900

    ButtonController

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,22 +8,44 @@ public class ButtonController : MonoBehaviour
 {
     public GameObject buttonPrefabs;
     public Transform layout;
-    public int btnCount;
+    public int btnCount = 48;
 
     void Start()
     {
-        btnCount = 47;
+        List<int> numbers = new List<int>();
+        for (int i = 1; i <= btnCount / 2; i++)
+        {
+            numbers.Add(i);
+            numbers.Add(i);
+        }
+        Shuffle(numbers);
+
         for (int i = 0; i < btnCount; i++)
         {
             GameObject newButton = Instantiate(buttonPrefabs, layout);
 
+            Text buttonText = newButton.GetComponentInChildren<Text>();
+            if (buttonText != null)
+            {
+                buttonText.text = numbers[i].ToString();
+            }
+        }
+    }
 
+    void Shuffle(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, list.Count);
+            int temp = list[i];
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
